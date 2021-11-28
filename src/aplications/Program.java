8a7371195869d0d55eec9976d476fdb176c55d26
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2021-11-28 08:05:38 -0300

    Criando a entidade Seller

--- src/aplications/Program.java
+++ src/aplications/Program.java
@@ -3,7 +3,10 @@
  */
 package aplications;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 /**
  * @author leo_dias
@@ -14,7 +17,10 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
